bb304be15607e9f1822a7d7462a6b62ba4ddcbd3
chuongtran <chuong.tran@infodatinc.com>
2024-05-21 15:10:24 -0500

    CICD-2: Modified Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home CICD-1 New</h1>
+      <h1>Home CICD-2</h1>
       <Link href="/about">About</Link>
     </div>
   );
